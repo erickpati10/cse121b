8c7ebb1cdc75fff17b57c321f3e71905c60c3d12
erickpati10 <pat21018@byui.edu>
2023-03-04 21:27:29 -0700

    updates

--- week02/scripts/task2.js
+++ week02/scripts/task2.js
@@ -8,12 +8,12 @@ const fullName = 'Erick Patino';
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
 
-document.querySelector('#name').innerHTML = `<h1>${fullName}</h1>`;
-document.querySelector('#name').innerText = `<h1>${fullName}</h1>`;
+
+document.querySelector('#name').innerText = fullName;
 
 // Step 3: declare and instantiate a variable to hold the current year
 
-const currentYear = new Date().getFullYear();
+const currentYear = 2023;
 
 // Step 4: place the value of the current year variable into the HTML file
 
@@ -22,7 +22,7 @@ document.querySelector('#year').innerHTML = currentYear;
 // Step 5: declare and instantiate a variable to hold the name of your picture
 
 const profileImage = 'images/picture_er.jpg';
-const profileAlt = 'My frofile picture'
+
 
 
 // Step 6: copy your image into the "images" folder
@@ -30,7 +30,7 @@ const profileAlt = 'My frofile picture'
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
 
 document.querySelector('img').setAttribute('src', profileImage);
-document.querySelector('img').setAttribute('alt', profileAlt);
+
 
 
 
@@ -38,44 +38,38 @@ document.querySelector('img').setAttribute('alt', profileAlt);
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-let myFood = ['Cheese', 'Apples', 'Pears', 'Steak' ];
+const myFoods = ['Cheese', 'Apples', 'Pears', 'Steak' ,'Chicken', 'Pasta' ];
 
-let allFood = ''
-
-for (let foodItem of myFood) {
-    allFood += `<span id="food-item">${foodItem},</span>`
-}
-
-console.log(allFood)
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector('#food').innerHTML = allFood
+document.querySelector('#food').textContent = myFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-const addFood = 'Chicken';
+const favFood = 'Chicken';
 
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-addFood.push('addFood')
+myFoods.push(favFood);
 
 // Step 5: repeat Step 2
-document.querySelector('#food').textContent = myFood;
+document.querySelector('#food').textContent = myFoods;
 
 
 // Step 6: remove the first element in the favorite foods array
-myFood.shift('cheese');
+myFoods.shift();
 
 
 // Step 7: repeat Step 2
-document.querySelector('#food').textContent = myFood;
+document.querySelector('#food').textContent = myFoods;
 
 
 // Step 8: remove the last element in the favorite foods array
-myFood.pop('chicken');
+myFoods.pop();
+
 
 
 // Step 7: repeat Step 2
-document.querySelector('#food').textContent = myFood;
+document.querySelector('#food').textContent = myFoods;
 
 
 
