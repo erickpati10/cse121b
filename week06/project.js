a79160ebb1d120c37b43a6b61de3602724660717
erickpati10 <pat21018@byui.edu>
2023-03-16 22:54:17 -0600

    updates

---
+++ week06/project.js
@@ -0,0 +1,38 @@
+let input =document.getElementById("add")
+let btn = document.getElementById("btn")
+let task = document.getElementById("task")
+
+
+
+btn.addEventListener('click', () =>{
+    let list = input.value;
+    if(list === ""){
+        alert('This field is required');
+
+    }else{
+        let text = document.createElement('li');
+        text.innerText = list;
+        task.appendChild(text)
+        input.value = '';
+    }
+    
+})  
+
+task.addEventListener('click', myTask => {
+    const element = myTask.target
+    if (element.style.getPropertyValue('text-decoration') == 'line-through') {
+        element.style.setProperty('text-decoration', 'none')
+    } else {
+        element.style.setProperty('text-decoration', 'line-through')
+    }
+})
+
+
+task.addEventListener('dblclick', myTask => {
+    myTask.target.remove()
+    
+   
+    
+
+})
+
