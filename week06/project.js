97a13763d2d93497c5b7fb83855c24e28b31a630
erickpati10 <pat21018@byui.edu>
2023-04-05 22:48:28 -0600

    updates
    Co-authored-by: Scott Glen Blanch <scottglenblanch@gmail.com>

--- week06/project.js
+++ week06/project.js
@@ -1,38 +1,64 @@
 
-
+let todoList = [];
 let input =document.getElementById("add")
 let btn = document.getElementById("btn")
-let task = document.getElementById("task")
 
 btn.addEventListener('click', () =>{
-    let list = input.value;
-    if(list === ""){
+    let inputValue = input.value;
+    if(inputValue === ""){
         alert('This field is required');
 
     }else{
-        let text = document.createElement('li');
-        text.innerText = list;
-        task.appendChild(text)
+        todoList.push(inputValue);
+        todoList.sort();
+
+        renderTodos();
         input.value = '';
-    }
+
     
+    }
 })  
 
-task.addEventListener('click', myTask => {
-    const element = myTask.target
-    if (element.style.getPropertyValue('text-decoration') == 'line-through') {
-        element.style.setProperty('text-decoration', 'none')
-    } else {
-        element.style.setProperty('text-decoration', 'line-through')
+function renderTodos() {
+    let unorderListContainer = document.querySelector(".list")
+
+    unorderListContainer.innerHTML = '';
+
+    for(let i = 0; i< todoList.length; i++) {
+        const todo = todoList[i];
+        const listElement = getListElement(todo, i);
+
+        unorderListContainer.appendChild(listElement)
     }
-})
+}
 
-
-task.addEventListener('dblclick', myTask => {
-    myTask.target.remove()
+function getListElement(todo, index) {
+    let liElement = document.createElement('li');
     
-   
-    
+    addClickEventsToListElement(liElement, index);
 
-})
+    
+    let span2 = document.createElement("span");
+    span2.innerHTML = todo;
+    
+    liElement.appendChild(span2);
+
+    return liElement;
+}
+
+function addClickEventsToListElement(listElement, index) {
+    listElement.addEventListener('click', event => {
+        if (listElement.style.getPropertyValue('text-decoration') == 'line-through') {
+            listElement.style.setProperty('text-decoration', 'none')
+        } else {
+            listElement.style.setProperty('text-decoration', 'line-through')
+        }
+    })
+
+    
+    listElement.addEventListener('dblclick', event => {
+        event.target.remove()
+        todoList.splice(index, index + 1)
+    })
+}
 
